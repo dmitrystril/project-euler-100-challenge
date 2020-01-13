e39829fc48d70a3411d084cfe910288053e26c50
Dmitry Stril <dmitry.stril@gmail.com>
2020-01-13 03:38:52 +0200

    add solution to 1-st task

--- src/main/component/tasks/task1/Task1.tsx
+++ src/main/component/tasks/task1/Task1.tsx
@@ -6,7 +6,30 @@ import StringConst from '../../../../constant/StringConst';
 
 const Root = styled.div`
   width: 100%;
+  height: 100%;
 `;
+
+const Result = styled.div`
+  margin: 20px 0;
+  font-weight: 500;
+`;
+
+const evaluateResult = () => {
+  const maxMultiple = 1000;
+
+  const firstMultiplier = 3;
+  const secondMultiplier = 5;
+
+  let sumOfMultiples = 0;
+
+  for (let i = 0; i < maxMultiple; i++) {
+    if ((i % firstMultiplier == 0) || (i % secondMultiplier == 0)) {
+      sumOfMultiples += i;
+    }
+  }
+
+  return sumOfMultiples;
+};
 
 const Task1 = () => {
   return (
@@ -20,7 +43,7 @@ const Task1 = () => {
         date={new Date('2020-01-10')}
       />
 
-      Task # 1
+      <Result>Result: {evaluateResult()}</Result>
     </Root>
   );
 };
